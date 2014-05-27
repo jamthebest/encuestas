fd22dea6317b919f249d354633e6c3b4ff4ec839
jamthebest <javier.midence@unitec.edu>
2014-05-27 11:38:34 -0600

    Final

---
+++ app/database/migrations/2014_05_27_173216_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuarios', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('username',50);
+	    $table->string('correo',128);
+	    $table->string('password',62);
+	    $table->string('tipo',45);
+	    $table->integer('activo');
+	    $table->string('remember_token');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('usuarios');
+	}
+
+}
